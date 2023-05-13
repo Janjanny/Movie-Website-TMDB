3fc82b580d0538e214a0567c2b3e92e1411141b9
Chris Jan G. Beboso <janjanbeboso0912@gmail.com>
2023-05-13 15:21:56 +0800

    Backdrop

---
+++ src/components/Backdrop.jsx
@@ -0,0 +1,9 @@
+import "../styles/Backdrop.css";
+function Backdrop() {
+  return (
+    <div>
+      <div className="backdrop"></div>
+    </div>
+  );
+}
+export default Backdrop;
---
+++ src/pages/Featured.jsx
@@ -0,0 +1,11 @@
+import { Backdrop } from "../components";
+
+function Featured() {
+  return (
+    <div>
+      <Backdrop />
+      <h2>Avatar</h2>
+    </div>
+  );
+}
+export default Featured;
