60b4567d8bf0bfdab4f76c3e57bc52b600f0d3fc
Chris Jan G. Beboso <janjanbeboso0912@gmail.com>
2023-05-17 14:51:17 +0800

    test slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,40 +1,44 @@
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
+import "../styles/Slider.css";
 
 // Import Swiper styles
 import "swiper/css";
 
 function Slider() {
   return (
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log("slide change")}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <div className="slide-container">
-        <SwiperSlide>
-          <div className="slide">
-            <img src="src/assets/images/sample2.jpg" alt="" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="slide">
-            <img src="src/assets/images/sample3.jpg" alt="" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="slide">
-            <img src="src/assets/images/sample4.jpg" alt="" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="slide">
-            <img src="src/assets/images/sample5.jpg" alt="" />
-          </div>
-        </SwiperSlide>
-      </div>
-    </Swiper>
+    <div className="swiper-container">
+      <p className="ftrd">featured movies</p>
+      <Swiper
+        spaceBetween={150}
+        slidesPerView={2}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <div className="slide-container">
+          <SwiperSlide>
+            <div className="slide">
+              <img src="src/assets/images/sample2.jpg" alt="" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="slide">
+              <img src="src/assets/images/sample3.jpg" alt="" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="slide">
+              <img src="src/assets/images/sample4.jpg" alt="" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="slide">
+              <img src="src/assets/images/sample5.jpg" alt="" />
+            </div>
+          </SwiperSlide>
+        </div>
+      </Swiper>
+    </div>
   );
 }
 export default Slider;
