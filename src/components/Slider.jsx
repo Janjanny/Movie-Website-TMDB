b7d8370fd9c0f7074e36f808c19c1ab8f0ecad4b
Chris Jan G. Beboso <janjanbeboso0912@gmail.com>
2023-05-17 22:37:15 +0800

    change swiper to coverflow

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,19 +1,37 @@
 // Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import "../styles/Slider.css";
 
 // Import Swiper styles
 import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+// import required modules
+import { EffectCoverflow, Pagination } from "swiper";
 
 function Slider() {
+  const swiper = useSwiper();
+
   return (
     <div className="swiper-container">
       <p className="ftrd">featured movies</p>
       <Swiper
-        spaceBetween={150}
-        slidesPerView={2}
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"2"}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        loop={true}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
       >
         <div className="slide-container">
           <SwiperSlide>
