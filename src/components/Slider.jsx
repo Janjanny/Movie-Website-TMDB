1b0c9dfa41c74e40c7d45502cb4296f7ede7e034
Chris Jan G. Beboso <janjanbeboso0912@gmail.com>
2023-05-18 23:16:40 +0800

    slider still not working, need redo

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import { useState, React } from "react";
 
 // Import Swiper React components
-import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import "../styles/Slider.css";
 
 // Import Swiper styles
@@ -10,10 +10,23 @@ import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 
 // import required modules
-import { EffectCoverflow, Pagination } from "swiper";
+import SwiperCore, { EffectCoverflow, Pagination } from "swiper";
+
+//import movie data
+import { featured_movies } from "../data";
+
+SwiperCore.use([EffectCoverflow, Pagination]);
 
 function Slider() {
-  const swiper = useSwiper();
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const slideClick = (slideIndex) => {
+    setCurrentSlide(slideIndex);
+    swiper.slideTo(slideIndex);
+  };
+
+  const swiper = new Swiper();
+
   return (
     <div className="swiper-container">
       <p className="ftrd">featured movies</p>
@@ -34,28 +47,21 @@ function Slider() {
         pagination={true}
         modules={[EffectCoverflow, Pagination]}
         className="mySwiper"
+        onInit={(swiperInstance) => {
+          swiperInstance.controller.control = swiper;
+        }}
+        onSlideChange={(swiper) => setCurrentSlide(swiper.activeIndex)}
       >
         <div className="slide-container">
-          <SwiperSlide>
-            <div className="slide">
-              <img src="src/assets/images/sample2.jpg" alt="" />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="slide">
-              <img src="src/assets/images/sample3.jpg" alt="" />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="slide">
-              <img src="src/assets/images/sample4.jpg" alt="" />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="slide">
-              <img src="src/assets/images/sample5.jpg" alt="" />
-            </div>
-          </SwiperSlide>
+          {featured_movies.map((slide, index) => (
+            <SwiperSlide key={slide.id} onClick={() => slideClick(index)}>
+              <div
+                className={`slide ${index === currentSlide ? "active" : ""}`}
+              >
+                <img src={slide.image} alt="" />
+              </div>
+            </SwiperSlide>
+          ))}
         </div>
       </Swiper>
     </div>
--- src/data.js
+++ src/data.js
@@ -1,12 +1,63 @@
+// put some async await here to fetch data when everything is okay
+
 export const featured_movies = [
+  // {
+  //   id: 0,
+  //   image: "src/assets/images/sample1.jpg",
+  //   title: "Avatar: The Way of the Water",
+  //   rating: 77,
+  //   date: "12/14/2022(PH)",
+  //   genre: "Science Fiction, Adventure, Action",
+  //   duration: "3h 12m",
+  //   summary:
+  //     "Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+  // },
+
   {
-    image: "src/assets/images/sample1.jpg",
-    title: "Avatar: The Way of the Water",
+    id: 1,
+    image: "src/assets/images/sample2.jpg",
+    title: "Evil Dead Rise",
     rating: 77,
-    date: "12/14/2022(PH)",
-    genre: "Science Fiction, Adventure, Action",
-    duration: "3h 12m",
+    date: "04/21/2023 (US)",
+    genre: "Horror, Thriller, Fantasy",
+    duration: "1h 36m",
     summary:
-      "Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+      "Three siblings find an ancient vinyl that gives birth to bloodthirsty demons that run amok in a Los Angeles apartment building and thrusts them into a primal battle for survival as they face the most nightmarish version of family imaginable.",
+  },
+
+  {
+    id: 2,
+    image: "src/assets/images/sample3.jpg",
+    title: "The Super Mario Bros.",
+    rating: 76,
+    date: "04/19/2023 (PH)",
+    genre: "Animation, Family, Adventure, Fantasy, Comedy",
+    duration: "1h 32m",
+    summary:
+      "While working underground to fix a water main, Brooklyn plumbers—and brothers—Mario and Luigi are transported down a mysterious pipe and wander into a magical new world. But when the brothers are separated, Mario embarks on an epic quest to find Luigi.",
+  },
+
+  {
+    id: 3,
+    image: "src/assets/images/sample4.jpg",
+    title: "Guardians of the Galaxy Vol. 3",
+    rating: 82,
+    date: "05/05/2023 (US)",
+    genre: "Science Fiction, Adventure, Action",
+    duration: "2h 30m",
+    summary:
+      "Peter Quill, still reeling from the loss of Gamora, must rally his team around him to defend the universe along with protecting one of their own. A mission that, if not completed successfully, could quite possibly lead to the end of the Guardians as we know them.",
+  },
+
+  {
+    id: 4,
+    image: "src/assets/images/sample5.jpg",
+    title: "Fast X",
+    rating: 67,
+    date: "05/19/2023 (US)",
+    genre: "Action, Crime",
+    duration: "2h 22m",
+    summary:
+      "Over many missions and against impossible odds, Dom Toretto and his family have outsmarted, out-nerved and outdriven every foe in their path. Now, they confront the most lethal opponent they've ever faced: A terrifying threat emerging from the shadows of the past who's fueled by blood revenge, and who is determined to shatter this family and destroy everything—and everyone—that Dom loves, forever.",
   },
 ];
