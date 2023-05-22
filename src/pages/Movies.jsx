e46e0b59c7c271bc5c8c5e0db48d2be8b0291e64
Chris Jan G. Beboso <janjanbeboso0912@gmail.com>
2023-05-22 15:43:58 +0800

    search bar functions iplemented

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,6 +1,32 @@
+import { useState } from "react";
 import "../styles/Movies.css";
 
 function Movies() {
+  //for setting the value of the input
+  const [searchValue, setSearchValue] = useState("");
+
+  // for setting the value of the dropdown in search bar
+  const [dropdownValue, setDropdownValue] = useState("Movies");
+
+  //for toggling the visibility of the dropdown menu
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
+  //function for setting the searchValue
+  const handleInputChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  //function for setting the dropdown value
+  const handleDropdownValue = (item) => {
+    setDropdownValue(item);
+    setDropdownVisible(false);
+  };
+
+  //function for setting the dropdown visibility
+  const handleDropdownVisibility = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
   return (
     <>
       <div className="movie-container">
@@ -8,15 +34,46 @@ function Movies() {
           <div className="icon">
             <ion-icon name="search-outline"></ion-icon>
           </div>
-          <input type="search" placeholder="Search..." />
+          <input
+            type="search"
+            placeholder="Search..."
+            value={searchValue}
+            onChange={handleInputChange}
+          />
           <hr className="search-bar-divider" />
-          <div className="dropdown-menu">
-            <ul>
-              <li>Movies</li>
-              <li>Series</li>
-              <li>People</li>
-            </ul>
+          <div className="search-location">
+            <p>{dropdownValue}</p>
           </div>
+          <div className="toggle-btn" onClick={handleDropdownVisibility}>
+            <ion-icon name="chevron-down-outline"></ion-icon>
+          </div>
+          {dropdownVisible && (
+            <div className="dropdown-menu">
+              <ul>
+                <li
+                  onClick={() => {
+                    handleDropdownValue("Movies");
+                  }}
+                >
+                  Movies
+                </li>
+                <li
+                  onClick={() => {
+                    handleDropdownValue("Series");
+                  }}
+                >
+                  Series
+                </li>
+                <li
+                  onClick={() => {
+                    handleDropdownValue("People");
+                  }}
+                >
+                  People
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </div>
     </>
