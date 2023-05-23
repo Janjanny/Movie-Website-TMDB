b992133ca3ba3e346d01753be74b537661ec0f75
Chris Jan G. Beboso <janjanbeboso0912@gmail.com>
2023-05-24 00:38:43 +0800

    card not done styles

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -29,6 +29,31 @@ function Movies() {
           ))}
         </div>
         <div className="red-line"></div>
+
+        {/* movie section */}
+        <div className="movie-grid">
+          <div className="movie-card">
+            <div className="movie-card-image">
+              <img src="src/assets/images/111.jpg" alt="" />
+            </div>
+            <div className="movie-card-info">
+              <p className="title">Avatar: The Way of the Water</p>
+              <ul>
+                <li>2h 1min</li>
+                <li>Science Fiction, Drama</li>
+                <li>2019</li>
+              </ul>
+              <p className="summary">
+                Set more than a decade after the events of the first film, learn
+                the story of the Sully family (Jake, Neytiri, and their kids),
+                the trouble that follows them, the lengths they go to keep each
+                other safe, the battles they fight to stay alive, and the
+                tragedies they endure.
+              </p>
+              <button className="button">See More</button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
