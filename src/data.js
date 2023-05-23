be54de29d4b71b144394743bc95b5433f879cc01
Chris Jan G. Beboso <janjanbeboso0912@gmail.com>
2023-05-23 23:09:01 +0800

    done category buttons

--- src/data.js
+++ src/data.js
@@ -61,3 +61,11 @@ export const featured_movies = [
       "Over many missions and against impossible odds, Dom Toretto and his family have outsmarted, out-nerved and outdriven every foe in their path. Now, they confront the most lethal opponent they've ever faced: A terrifying threat emerging from the shadows of the past who's fueled by blood revenge, and who is determined to shatter this family and destroy everything—and everyone—that Dom loves, forever.",
   },
 ];
+
+export const category = [
+  "All Movies",
+  "Popular",
+  "Now Playing",
+  "Upcoming",
+  "Top Rated",
+];
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,12 +1,33 @@
 import { useState } from "react";
 import "../styles/Movies.css";
 import { SearchBar } from "../components";
+import { category } from "../data";
 
 function Movies() {
+  const [activeButton, setActiveButton] = useState(null);
+
+  const handleCtgry = (id) => {
+    setActiveButton(id);
+  };
+
   return (
     <>
       <div className="movie-container">
         <SearchBar />
+        <div className="category-buttons">
+          {category.map((item, i) => (
+            <button
+              onClick={() => {
+                handleCtgry(i);
+              }}
+              className={`category-btn ${
+                activeButton === i ? "category-active" : ""
+              }`}
+            >
+              {item}
+            </button>
+          ))}
+        </div>
       </div>
     </>
   );
