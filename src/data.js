3b402f87fc5e89cdc3ad4a7cd8e8be6f473b8ebc
Chris Jan G. Beboso <janjanbeboso0912@gmail.com>
2023-05-24 14:38:33 +0800

    create movie object

--- src/data.js
+++ src/data.js
@@ -69,3 +69,62 @@ export const category = [
   "Upcoming",
   "Top Rated",
 ];
+
+export const movieList = [
+  {
+    image: "src/assets/images/posters/avatar-poster.jpg",
+    title: "Avatar: The Way of the Water",
+    duration: "2h 1m",
+    genre: "Science Fiction, Drama",
+    year: 2019,
+    summary:
+      "Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+  },
+  {
+    image: "src/assets/images/posters/mario-poster.jpg",
+    title: "The Super Mario Bros",
+    duration: "1h 32m",
+    genre: "Animation, Family",
+    year: 2023,
+    summary:
+      "While working underground to fix a water main, Brooklyn plumbers—and brothers—Mario and Luigi are transported down a mysterious pipe and wander into a magical new world. But when the brothers are separated, Mario embarks on an epic quest to find Luigi.",
+  },
+  {
+    image: "src/assets/images/posters/fast-poster.jpg",
+    title: "Fast X",
+    duration: "2h 22m",
+    genre: "Action, Adventure",
+    year: 2023,
+    summary:
+      "Over many missions and against impossible odds, Dom Toretto and his family have outsmarted, out-nerved and outdriven every foe in their path. Now, they confront the most lethal opponent they've ever faced: A terrifying threat emerging from the shadows of the past who's fueled by blood revenge, and who is determined to shatter this family and destroy everything—and everyone—that Dom loves, forever.",
+  },
+  {
+    image: "src/assets/images/posters/evil-poster.jpg",
+    title: "Evil Dead Rise",
+    duration: "1h 36m",
+    genre: "Thriller, Horror",
+    year: 2023,
+    summary:
+      "Three siblings find an ancient vinyl that gives birth to bloodthirsty demons that run amok in a Los Angeles apartment building and thrusts them into a primal battle for survival as they face the most nightmarish version of family imaginable.",
+  },
+
+  {
+    image: "src/assets/images/posters/sisu-poster.jpg",
+    title: "Sisu",
+    duration: "1h 31m",
+    genre: "Action, War",
+    year: 2023,
+    summary:
+      "Deep in the wilderness of Lapland, Aatami Korpi is searching for gold but after he stumbles upon Nazi patrol, a breathtaking and gold-hungry chase through the destroyed and mined Lapland wilderness begins.",
+  },
+
+  {
+    image: "src/assets/images/posters/ant-man-poster.jpg",
+    title: "Ant-Man and the Wasp: Quantumania",
+    duration: "2h 5m",
+    genre: "Action, Adventure",
+    year: 2023,
+    summary:
+      "Super-Hero partners Scott Lang and Hope van Dyne, along with with Hope's parents Janet van Dyne and Hank Pym, and Scott's daughter Cassie Lang, find themselves exploring the Quantum Realm, interacting with strange new creatures and embarking on an adventure that will push them beyond the limits of what they thought possible.",
+  },
+];
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "../styles/Movies.css";
 import { SearchBar } from "../components";
-import { category } from "../data";
+import { category, movieList } from "../data";
 
 function Movies() {
   const [activeButton, setActiveButton] = useState(null);
@@ -23,6 +23,7 @@ function Movies() {
               className={`category-btn ${
                 activeButton === i ? "category-active" : ""
               }`}
+              key={i}
             >
               {item}
             </button>
@@ -32,27 +33,23 @@ function Movies() {
 
         {/* movie section */}
         <div className="movie-grid">
-          <div className="movie-card">
-            <div className="movie-card-image">
-              <img src="src/assets/images/111.jpg" alt="" />
+          {movieList.map((movie, i) => (
+            <div className="movie-card" key={i}>
+              <div className="movie-card-image">
+                <img src={movie.image} alt="" />
+              </div>
+              <div className="movie-card-info">
+                <p className="title">{movie.title}</p>
+                <ul>
+                  <li>{movie.duration}</li>
+                  <li>{movie.genre}</li>
+                  <li>{movie.year}</li>
+                </ul>
+                <p className="summary">{movie.summary}</p>
+                <button className="button">See More</button>
+              </div>
             </div>
-            <div className="movie-card-info">
-              <p className="title">Avatar: The Way of the Water</p>
-              <ul>
-                <li>2h 1min</li>
-                <li>Science Fiction, Drama</li>
-                <li>2019</li>
-              </ul>
-              <p className="summary">
-                Set more than a decade after the events of the first film, learn
-                the story of the Sully family (Jake, Neytiri, and their kids),
-                the trouble that follows them, the lengths they go to keep each
-                other safe, the battles they fight to stay alive, and the
-                tragedies they endure.
-              </p>
-              <button className="button">See More</button>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
